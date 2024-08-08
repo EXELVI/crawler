3f4225b5d91c5357cf3bcdb0efeb74163cc5ec20
EXELVI <exelvi@outlook.com>
2024-08-08 12:23:27 +0200

    feat: device crawling, ecc

--- index.js
+++ index.js
@@ -28,19 +28,38 @@ app.get('/', (req, res) => {
 
 let screenshots = [];
 
+/**
+ * 
+ * @param {puppeteer.Page} page  
+ */
+async function cookieAccept(page) {
+  if (page.url().includes('youtube')) {
+    // aria-label="Accetta l'utilizzo dei cookie e di altri dati per le finalità descritte"
+    await page.waitForSelector('button[aria-label="Accetta l\'utilizzo dei cookie e di altri dati per le finalità descritte"]').catch(() => console.log('Cookie button not found')).then(async () => {
+      await page.click('button[aria-label="Accetta l\'utilizzo dei cookie e di altri dati per le finalità descritte"]')
+    });
+  }
+
+}
+
+var closed = false;
+
 io.on('connection', socket => {
   console.log('Client connected');
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-    screenshots.forEach(screenshot => {
+    if (!closed) screenshots.forEach(screenshot => {
       if (screenshot.id === socket.id) {
+        screenshots = screenshots.filter(s => s !== screenshot);
+        console.log(`Removing screenshot: ${screenshot.name}`);
         fs.unlinkSync(path.join(__dirname, 'screenshots', screenshot.name));
       }
     });
   });
 
-  socket.on("crawl", async (url) => {
+  socket.on("crawl", async (data) => {
+    let { url, method } = data;
     const screenshotsDir = path.join(__dirname, 'screenshots');
     if (!fs.existsSync(screenshotsDir)) {
       fs.mkdirSync(screenshotsDir);
@@ -49,13 +68,31 @@ io.on('connection', socket => {
       headless: true,
       executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
       //userDataDir: 'C:\\Users\\exelv\\AppData\Local\\BraveSoftware\\Brave-Browser\\User Data',
-      defaultViewport: { width: 1920, height: 1080 }
-  });
-  socket.emit('loading', 'Loading page...');
+      defaultViewport: method === 'mobile' ? { width: 375, height: 667 } : { width: 1920, height: 1080 }
+    });
+
+  
+    
+    socket.emit('loading', 'Loading page...');
     const page = await browser.newPage()
     await page.goto(url);
 
-    await page.reload(); // Awoiding youtube cookies popup
+    if (method == "mobile") {
+      await page.emulate(puppeteer.KnownDevices['iPhone 15 Pro Max']);      
+    }
+
+    socket.emit('progress', 33);
+
+    // await page.reload();
+
+
+    await page.waitForSelector('body');
+
+    socket.emit('progress', 66);
+    await cookieAccept(page);
+
+    socket.emit('progress', 100);
+
 
     let time = new Date().getTime();
 
@@ -65,15 +102,17 @@ io.on('connection', socket => {
       screenshots: []
     }
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       const screenshotPath = path.join(screenshotsDir, `${time}screenshot-${i}.png`);
       screenshots.push({ id: socket.id, name: `${time}screenshot-${i}.png` });
       await page.screenshot({ path: screenshotPath });
       console.log(`Screenshot saved: ${screenshotPath}`);
       infos.screenshots.push(`screenshots/${time}screenshot-${i}.png`);
+      socket.emit('progress', i * 10);
       await new Promise(resolve => setTimeout(resolve, 500));
     }
 
+    socket.emit('progress', 100);
     socket.emit('crawl', infos);
 
     await browser.close();
@@ -82,5 +121,18 @@ io.on('connection', socket => {
 
 server.listen(3000, () => {
   console.log(`Server running on port ${server.address().port}`);
+});
+
+process.on("SIGINT", () => {
+  closed = true;
+  server.close(() => {
+    console.log("Server closed");
+  });
+  console.log("Cleaning up...");
+  fs.rmSync(path.join(__dirname, 'screenshots'), { recursive: true })
+  console.log("Screenshots deleted");
+  fs.mkdirSync(path.join(__dirname, 'screenshots'));
+  console.log("Screenshots directory created");
+  process.exit(0);
 });
 
