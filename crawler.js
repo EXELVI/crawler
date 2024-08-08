f79d9e4807b4c54b96c4c1c51f7f325a44c4ed40
EXELVI <exelvi@outlook.com>
2024-08-08 10:57:20 +0200

    feat: Added socket, main page crawling

--- crawler.js
+++ crawler.js
@@ -3,18 +3,23 @@ const fs = require('fs');
 const path = require('path');
 
 const startCrawling = async () => {
-    const url = 'https://exelvi.github.io';
+    const url = 'https://youtu.be/dQw4w9WgXcQ';
     const screenshotsDir = path.join(__dirname, 'screenshots');
     if (!fs.existsSync(screenshotsDir)) {
         fs.mkdirSync(screenshotsDir);
     }
 
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+        //userDataDir: 'C:\\Users\\exelv\\AppData\Local\\BraveSoftware\\Brave-Browser\\User Data',
+        defaultViewport: { width: 1920, height: 1080 }
     });
-    const page = await browser.newPage();
+    const page = await browser.newPage()
     await page.goto(url);
+
+
+    await page.reload(); // Awoiding youtube cookies popup
 
     for (let i = 0; i < 5; i++) {
         const screenshotPath = path.join(screenshotsDir, `screenshot-${i}.png`);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const path = require('path');
 const socketIo = require('socket.io');
 const fs = require('fs');
+const { url } = require('inspector');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,21 +21,66 @@ app.use("/screenshots", express.static(path.join(__dirname, 'screenshots')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    const screenshotsDir = path.join(__dirname, 'screenshots');
-    const screenshots = fs.readdirSync(screenshotsDir).map(file => `screenshots/${file}`);
-    res.render('index', { screenshots });
+  const screenshotsDir = path.join(__dirname, 'screenshots');
+  const screenshots = fs.readdirSync(screenshotsDir).map(file => `screenshots/${file}`);
+  res.render('index', { screenshots });
 });
 
-io.on('connection', socket => {
-    console.log('Client connected');
+let screenshots = [];
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
+io.on('connection', socket => {
+  console.log('Client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    screenshots.forEach(screenshot => {
+      if (screenshot.id === socket.id) {
+        fs.unlinkSync(path.join(__dirname, 'screenshots', screenshot.name));
+      }
     });
+  });
+
+  socket.on("crawl", async (url) => {
+    const screenshotsDir = path.join(__dirname, 'screenshots');
+    if (!fs.existsSync(screenshotsDir)) {
+      fs.mkdirSync(screenshotsDir);
+    }
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+      //userDataDir: 'C:\\Users\\exelv\\AppData\Local\\BraveSoftware\\Brave-Browser\\User Data',
+      defaultViewport: { width: 1920, height: 1080 }
+  });
+  socket.emit('loading', 'Loading page...');
+    const page = await browser.newPage()
+    await page.goto(url);
+
+    await page.reload(); // Awoiding youtube cookies popup
+
+    let time = new Date().getTime();
+
+    let infos = {
+      title: await page.title(),
+      url: url,
+      screenshots: []
+    }
+
+    for (let i = 0; i < 5; i++) {
+      const screenshotPath = path.join(screenshotsDir, `${time}screenshot-${i}.png`);
+      screenshots.push({ id: socket.id, name: `${time}screenshot-${i}.png` });
+      await page.screenshot({ path: screenshotPath });
+      console.log(`Screenshot saved: ${screenshotPath}`);
+      infos.screenshots.push(`screenshots/${time}screenshot-${i}.png`);
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+
+    socket.emit('crawl', infos);
+
+    await browser.close();
+  });
 })
 
 server.listen(3000, () => {
-    console.log(`Server running on port ${server.address().port}`);
+  console.log(`Server running on port ${server.address().port}`);
 });
 
- 
